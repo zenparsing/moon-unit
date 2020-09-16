b08a0d04e894212ae1b172bfb434aa8ed1eb8895
zenparsing <zenparsing@gmail.com>
2020-09-16 17:50:27 -0400

    Add some more tests

--- test/basics.js
+++ test/basics.js
@@ -5,4 +5,13 @@ describe('Some simple tests', () => {
   it('should run a test', () => {
     assert.ok(true);
   });
+
+  it('should show an error when a test fails', () => {
+    assert.ok(false);
+  });
+
+  it('should run async tests', async () => {
+    await 1;
+    assert.ok(true);
+  });
 });
